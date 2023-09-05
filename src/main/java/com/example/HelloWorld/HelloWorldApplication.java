2cb19052013c48a322c857e9a8598b9d041bb975
anumtdy <128569432+anumtdy@users.noreply.github.com>
2023-09-05 12:18:35 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -8,7 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Hello !");
 	}
 
 }
